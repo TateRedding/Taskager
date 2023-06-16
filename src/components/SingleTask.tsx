9ec49f67bb20a490fc7c9e75509df67b432db012
Tate Redding <tateredding@gmail.com>
2023-06-16 13:44:42 -0600

    added functionality of edit/delete/mark as done task buttons

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../styling/SingleTask.css"
 import { Task } from "../model";
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
@@ -11,17 +11,68 @@ interface Props {
 }
 
 const SingleTask: React.FC<Props> = ({ task, tasks, setTasks }) => {
+    const [edit, setEdit] = useState<boolean>(false);
+    const [editTaskInput, setEditTaskInput] = useState<string>(task.task);
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const completeTask = (id: number) => {
+        setTasks(tasks.map(task => {
+            return task.id === id ?
+                { ...task, isDone: !task.isDone }
+                :
+                task
+        }));
+    };
+
+    const deleteTask = (id: number) => {
+        setTasks(tasks.filter(task => task.id !== id));
+    };
+
+    const editTask = (event: React.FormEvent, id: number) => {
+        event.preventDefault();
+        setTasks(tasks.map(task => {
+            return task.id === id ?
+                { ...task, task: editTaskInput }
+                :
+                task
+        }));
+        setEdit(false);
+    };
+
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, [edit]);
+
     return (
-        <form className="single-task">
-            <span className="single-task-text">{task.task}</span>
+        <form className="single-task" onSubmit={(event) => editTask(event, task.id)}>
+            {
+                edit ?
+                    <input
+                        ref={inputRef}
+                        className="single-task-text"
+                        value={editTaskInput}
+                        onChange={(event) => setEditTaskInput(event.target.value)}
+                    />
+                    :
+                    task.isDone ?
+                        <s className="single-task-text">{task.task}</s>
+                        :
+                        <span className="single-task-text">{task.task}</span>
+            }
+
             <div>
-                <span className="icon">
+                <span className="icon" onClick={() => {
+                    if (!edit && !task.isDone) {
+                        setEdit(!edit);
+                    };
+                }}>
                     <AiFillEdit />
                 </span>
-                <span className="icon">
+                <span className="icon" onClick={() => deleteTask(task.id)}>
                     <AiFillDelete />
                 </span>
-                <span className="icon">
+                <span className="icon" onClick={() => completeTask(task.id)}>
                     <MdDone />
                 </span>
             </div>
